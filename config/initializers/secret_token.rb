0e37ccd9205c2a6af1a082123836ddf26bd32d13
Ashish Upadhyay <ashish.upadhyaye@gmail.com>
2014-02-25 00:38:08 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StrollRailsExample::Application.config.secret_key_base = '1ca7676fdddfb0ab5f4004e8e6679805c73a8465f7b4ef62d9c5286f743e88f34de4aa1994d4a1ccd4780e775a610c2bc2534ab3e166cd5a93f7d97f7fabb46f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StrollRailsExample::Application.config.session_store :cookie_store, key: '_stroll-rails-example_session'
